fd784af38e2904a96897d84d06937d7bb231ee72
cristianhoyos66 <cristianhoyos66@gmail.com>
2023-07-26 12:38:16 -0500

    Create GRPC Project

---
+++ grpc-flix/user-service/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.cris;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
